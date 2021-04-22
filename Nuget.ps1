d2732a494e536c411af440afb0fd64845c0dfdcc
Albie <admin@dragonfruit.network>
2021-04-22 17:17:48 +0100

    Update Nuget.ps1

--- Nuget.ps1
+++ Nuget.ps1
@@ -1,22 +1,22 @@
-﻿param ([string] $ApiKey, [string]$Suffix = "")
+param ([string] $ApiKey, [string]$Suffix = "")
 
-#versioning info
+# versioning info
 $VERSION = "$(Get-Date -UFormat "%Y.%m%d").$($env:GITHUB_RUN_NUMBER)$($Suffix)"
 $WORKINGDIR = Get-Location
 
-#Build files
+# nuget clear & restore
+nuget locals all -clear
+nuget restore
 
+# build files
 Write-Output "Building OnionFruit.Status Version $VERSION"
-dotnet restore
 dotnet build -c Release /p:PackageVersion=$VERSION /p:Version=$VERSION
 
-#pack into nuget files with the suffix if we have one
-
+# pack into nuget files with the suffix if we have one
 Write-Output "Publishing OnionFruit.Status Version $VERSION"
 dotnet pack ".\DragonFruit.OnionFruit.Status\DragonFruit.OnionFruit.Status.csproj" -o $WORKINGDIR -c Release -p:PackageVersion=$VERSION
 
-#recursively push all nuget files created
-
+# recursively push all nuget files created
 Get-ChildItem -Path $WORKINGDIR -Filter *.nupkg -Recurse -File -Name | ForEach-Object {
     dotnet nuget push $_ --api-key $ApiKey --source https://api.nuget.org/v3/index.json --force-english-output
 }
