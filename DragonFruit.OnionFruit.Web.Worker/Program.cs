076d8857aac37482695ba59aa48fd47ba5a58555
Albie Spriddell <albion@dragonfruit.network>
2023-09-18 11:38:52 +0100

    add index generation

--- DragonFruit.OnionFruit.Web.Worker/Program.cs
+++ DragonFruit.OnionFruit.Web.Worker/Program.cs
@@ -1,10 +1,16 @@
+using System;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using DragonFruit.Data;
 using DragonFruit.OnionFruit.Web.Worker.Clients;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Redis.OM;
 using Redis.OM.Contracts;
+using Redis.OM.Modeling;
 using StackExchange.Redis;
 
 namespace DragonFruit.OnionFruit.Web.Worker;
@@ -16,6 +22,11 @@ public static class Program
         var host = Host.CreateDefaultBuilder(args)
             .ConfigureServices(ConfigureServices)
             .Build();
+
+        using (var scope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+        {
+            await ValidateRedisStructures(scope.ServiceProvider).ConfigureAwait(false);
+        }
 
         await host.RunAsync().ConfigureAwait(false);
     }
@@ -31,5 +42,32 @@ public static class Program
 
         // timed service
         services.AddHostedService<Worker>();
+    }
+
+    private static async Task ValidateRedisStructures(IServiceProvider services)
+    {
+        var config = services.GetRequiredService<IConfiguration>();
+        var redis = services.GetRequiredService<IRedisConnectionProvider>();
+        var logger = services.GetRequiredService<ILogger<IRedisConnectionProvider>>();
+
+        var createNewIndexes = config["REDIS_CREATE_INDEXES"]?.Equals("true", StringComparison.OrdinalIgnoreCase) ?? true;
+        var regenExistingIndexes = config["REDIS_REGEN_INDEXES"]?.Equals("true", StringComparison.OrdinalIgnoreCase) ?? false;
+
+        foreach (var type in Assembly.GetExecutingAssembly().ExportedTypes.Where(x => x.GetCustomAttribute<DocumentAttribute>() != null))
+        {
+            // drop index if regenerating
+            if (regenExistingIndexes)
+            {
+                logger.LogInformation("Dropping index for type {t}", type.Name);
+                await redis.Connection.DropIndexAsync(type).ConfigureAwait(false);
+            }
+
+            // create if enabled OR the index is being regenerated
+            if (regenExistingIndexes || createNewIndexes)
+            {
+                logger.LogInformation("Initialising index for type {t} (if not already present)", type.Name);
+                await redis.Connection.CreateIndexAsync(type).ConfigureAwait(false);
+            }
+        }
     }
 }
