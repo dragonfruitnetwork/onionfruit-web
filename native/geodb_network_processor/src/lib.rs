d666f017b4b66a60dc86b3458767aec9de08a7fd
Albie Spriddell <albion@dragonfruit.network>
2023-10-05 18:34:55 +0100

    add missing macro causing mangled values

--- native/geodb_network_processor/src/lib.rs
+++ native/geodb_network_processor/src/lib.rs
@@ -12,12 +12,12 @@ use std::os::raw::c_void;
 /// Represents a block of network addresses
 /// For simplicity, all addresses are treated as IPv6
 struct NetBlock {
-    pub network: Ipv6Network,
-    pub country_code: [u8; 2]
+    network: Ipv6Network,
+    country_code: [u8; 2]
 }
 
 /// Represents the info associated with a network block.
-#[derive(Clone, Eq, PartialEq)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 struct NetworkInfo {
     country_code: [u8; 2]
 }
@@ -85,7 +85,7 @@ pub extern "cdecl" fn sort_network_entries(ptr: *const c_void, length: usize, ou
 
 #[no_mangle]
 pub unsafe extern "cdecl" fn free_sort_result(ptr: *const InteropNetworkSortResult) {
-    let result = *ptr ;
+    let result = *ptr;
     drop(Vec::<InteropNetworkRange<u32>>::from_raw_parts(result.v4entries, result.v4count, result.v4count));
     drop(Vec::<InteropNetworkRange<u128>>::from_raw_parts(result.v6entries, result.v6count, result.v6count));
 }
