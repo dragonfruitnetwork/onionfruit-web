35a4c7c8f68e541805b5820f4dd65870bb049359
Albie Spriddell <albion@dragonfruit.network>
2023-10-06 11:46:21 +0100

    add native methods

---
+++ DragonFruit.OnionFruit.Web.Worker/Native/NativeMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DragonFruit.OnionFruit.Web.Worker.Native;
+
+public static class NativeMethods
+{
+    private const string LibraryName = "onionfruit_worker_native";
+
+    [DllImport(LibraryName, EntryPoint = "sort_network_entries", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void PerformNetworkSort([In, MarshalAs(UnmanagedType.LPArray)] NetworkEntry[] entries, nint length, out NetworkSortResult result);
+
+    [DllImport(LibraryName, EntryPoint = "free_sort_result", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ClearSortResult([In] ref NetworkSortResult ptr);
+}
---
+++ DragonFruit.OnionFruit.Web.Worker/Native/NativeStructs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DragonFruit.OnionFruit.Web.Worker.Native;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct IPv4NetworkRange
+{
+    public fixed byte start_address[4];
+    public fixed byte end_address[4];
+
+    public fixed byte country_code[2];
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public unsafe struct IPv6NetworkRange
+{
+    public fixed byte start_address[16];
+    public fixed byte end_address[16];
+
+    public fixed byte country_code[2];
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct NetworkEntry
+{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+    public byte[] network;
+    public byte cidr;
+        
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+    public byte[] country_code;
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct NetworkSortResult
+{
+    public IntPtr v4Entries;
+    public nint v4Count;
+
+    public IntPtr v6Entries;
+    public nint v6Count;
+}
