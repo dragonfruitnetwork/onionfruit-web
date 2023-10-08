76ec99c7a4bc62594f2f3784424fc02c8ab08b34
Albie Spriddell <albion@dragonfruit.network>
2023-10-08 14:05:34 +0100

    add usage annotations

--- DragonFruit.OnionFruit.Web.Worker/Generators/IDatabaseGenerator.cs
+++ DragonFruit.OnionFruit.Web.Worker/Generators/IDatabaseGenerator.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
 using DragonFruit.OnionFruit.Web.Worker.Storage;
+using JetBrains.Annotations;
 
 namespace DragonFruit.OnionFruit.Web.Worker.Generators;
 
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature, ImplicitUseTargetFlags.WithInheritors)]
 public interface IDatabaseGenerator
 {
     Task GenerateDatabase(Lazy<IDatabaseFileSink> fileSink);
--- DragonFruit.OnionFruit.Web.Worker/Sources/IDataSource.cs
+++ DragonFruit.OnionFruit.Web.Worker/Sources/IDataSource.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 
 namespace DragonFruit.OnionFruit.Web.Worker.Sources;
 
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature, ImplicitUseTargetFlags.WithInheritors)]
 public interface IDataSource
 {
     Task<bool> HasDataChanged(DateTimeOffset lastVersionDate);
