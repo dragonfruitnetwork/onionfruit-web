0b274d41daecc2e7385bc38768d4d85ed345a617
Albie Spriddell <albion@dragonfruit.network>
2023-10-12 16:58:02 +0100

    add litedb generator

---
+++ DragonFruit.OnionFruit.Web.Worker/Generators/OnionLiteDbGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using DragonFruit.OnionFruit.Web.Worker.Sources;
+using DragonFruit.OnionFruit.Web.Worker.Storage;
+using Google.Protobuf;
+using NetTools;
+
+namespace DragonFruit.OnionFruit.Web.Worker.Generators;
+
+/// <summary>
+/// A variant of the onion.db file without address ranges. Used on the DragonFruit website.
+/// </summary>
+public class OnionLiteDbGenerator : OnionDbGenerator
+{
+    public OnionLiteDbGenerator(OnionooDataSource onionoo, LocationDbSource locationDb)
+        : base(onionoo, locationDb)
+    {
+    }
+    
+    protected override OnionDb CreateBaseDb()
+    {
+        var baseDb = base.CreateBaseDb();
+        
+        baseDb.ClearGeoLicense();
+        baseDb.ClearGeoDirVersion();
+
+        return baseDb;
+    }
+
+    protected override void WriteCountryAddressRanges(OnionDbCountry country, IEnumerable<IPAddressRange> v4Ranges, IEnumerable<IPAddressRange> v6Ranges)
+    {
+        // litedb doesn't include address ranges
+    }
+
+    protected override void OnDatabaseGenerated(IFileSink fileSink, OnionDb database)
+    {
+        database.WriteTo(fileSink.CreateFile("onion.lite.db"));
+    }
+}
