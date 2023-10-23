8a8aa71f8b0ab4217e8f53b0195fea62e8653513
Albie Spriddell <albion@dragonfruit.network>
2023-10-23 14:34:37 +0100

    add basic asset download controller

---
+++ DragonFruit.OnionFruit.Web/Controllers/AssetDownloadController.cs
@@ -0,0 +1,53 @@
+// OnionFruit™ Web Copyright DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under Apache-2. Refer to the LICENSE file for more info
+
+using System;
+using System.Globalization;
+using DragonFruit.OnionFruit.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DragonFruit.OnionFruit.Web.Controllers;
+
+[Route("~/assets")]
+public class AssetDownloadController : ControllerBase
+{
+    private readonly LocalAssetStore _assetStore;
+
+    public AssetDownloadController(LocalAssetStore assetStore)
+    {
+        _assetStore = assetStore;
+    }
+
+    [HttpGet("{*assetPath}")]
+    public IActionResult ResolveAssetPath(string assetPath)
+    {
+        var versionedAsset = _assetStore.GetAssetInfo(assetPath);
+
+        if (versionedAsset == null)
+        {
+            return NotFound();
+        }
+
+        // check last-modified date
+        if (DateTimeOffset.TryParse(Request.Headers.IfModifiedSince, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out var clientModificationDate) && clientModificationDate >= versionedAsset.CreatedAt)
+        {
+            return StatusCode(304);
+        }
+
+        Response.Headers.LastModified = versionedAsset.CreatedAt.ToString("r");
+        return RedirectToAction("DownloadVersionedAsset", new {versionedAssetPath = versionedAsset.VersionedPath});
+    }
+
+    [HttpGet("dl/{*versionedAssetPath}")]
+    public IActionResult DownloadVersionedAsset(string versionedAssetPath)
+    {
+        var fs = _assetStore.GetReadableFileStream(versionedAssetPath);
+
+        if (fs == null)
+        {
+            return NotFound();
+        }
+
+        return File(fs, "application/octet-stream");
+    }
+}
