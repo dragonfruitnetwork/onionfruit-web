7f85578fab9875baac358780c670ae1a3f2fc31f
Albie Spriddell <albion@dragonfruit.network>
2024-05-01 11:26:37 +0100

    revert libraryimport changes

--- DragonFruit.OnionFruit.Web.Worker/Native/NativeMethods.cs
+++ DragonFruit.OnionFruit.Web.Worker/Native/NativeMethods.cs
@@ -2,20 +2,17 @@
 // Licensed under Apache-2. Refer to the LICENSE file for more info
 
 using System;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace DragonFruit.OnionFruit.Web.Worker.Native;
 
-public static partial class NativeMethods
+public static class NativeMethods
 {
     private const string LibraryName = "onionfruit_worker_native";
 
-    [LibraryImport(LibraryName, EntryPoint = "sort_network_entries")]
-    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    public static partial void PerformNetworkSort(in IntPtr entries, nint length, out NetworkSortResult result);
+    [DllImport(LibraryName, EntryPoint = "sort_network_entries", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void PerformNetworkSort(IntPtr entries, nint length, out NetworkSortResult result);
 
-    [LibraryImport(LibraryName, EntryPoint = "free_sort_result")]
-    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
-    public static partial void ClearSortResult(ref NetworkSortResult ptr);
+    [DllImport(LibraryName, EntryPoint = "free_sort_result", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void ClearSortResult(ref NetworkSortResult ptr);
 }
