73718f939868b1a43df24478b3aea954d47fe8ff
Albie Spriddell <albion@dragonfruit.network>
2024-11-30 09:33:12 +0000

    allow remote/local stores in controller

--- DragonFruit.OnionFruit.Web/Controllers/AssetDownloadController.cs
+++ DragonFruit.OnionFruit.Web/Controllers/AssetDownloadController.cs
@@ -4,28 +4,23 @@
 using System;
 using System.Globalization;
 using System.IO;
+using System.Threading.Tasks;
 using System.Web;
 using DragonFruit.OnionFruit.Web.Data;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 
 namespace DragonFruit.OnionFruit.Web.Controllers;
 
 [EnableCors]
-public class AssetDownloadController : ControllerBase
+public class AssetDownloadController(IRemoteAssetStore assetStore) : ControllerBase
 {
-    private readonly LocalAssetStore _assetStore;
-
-    public AssetDownloadController(LocalAssetStore assetStore)
-    {
-        _assetStore = assetStore;
-    }
-
     [HttpGet("~/assets/{*assetPath}")]
-    public IActionResult ResolveAssetPath(string assetPath)
+    public async Task<IActionResult> ResolveAssetPath(string assetPath)
     {
         assetPath = HttpUtility.UrlDecode(assetPath);
-        var versionedAsset = _assetStore.GetAssetInfo(assetPath);
+        var versionedAsset = await assetStore.GetAssetInfo(assetPath);
 
         if (versionedAsset == null)
         {
@@ -38,17 +33,27 @@ public class AssetDownloadController : ControllerBase
             return StatusCode(304);
         }
 
+        Response.Headers.ETag = new StringValues(versionedAsset.ETag);
         Response.Headers.LastModified = versionedAsset.CreatedAt.ToString("r");
 
-        var redirectUrl = Url.Action("DownloadVersionedAsset", new {versionedAssetPath = versionedAsset.VersionedPath});
-        return Redirect(HttpUtility.UrlDecode(redirectUrl));
+        if (assetStore is not IAssetStore && versionedAsset.VersionedPath.StartsWith("http"))
+        {
+            return Redirect(versionedAsset.VersionedPath);
+        }
+
+        return Redirect(HttpUtility.UrlDecode(Url.Action("DownloadVersionedAsset", new {versionedAssetPath = versionedAsset.VersionedPath})));
     }
 
     [HttpGet("~/asset-dl/{*versionedAssetPath}")]
     public IActionResult DownloadVersionedAsset(string versionedAssetPath)
     {
+        if (assetStore is not IAssetStore store)
+        {
+            return StatusCode(500);
+        }
+
         versionedAssetPath = HttpUtility.UrlDecode(versionedAssetPath);
-        var fs = _assetStore.GetReadableFileStream(versionedAssetPath);
+        var fs = store.GetReadableFileStream(versionedAssetPath);
 
         if (fs == null)
         {
